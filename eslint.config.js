8739bbadc0329eb04a7d9a0587a43e02b2e5a5b4
Chaosbeing <50495803+kaosbeing@users.noreply.github.com>
2025-02-16 12:40:58 +0100

    Reset (#4)

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import babelParser from '@babel/eslint-parser';
+
+export default [
+    {
+        ignores: ['node_modules/', 'dist/', 'build/']
+    },
+    {
+        languageOptions: {
+            parser: babelParser,
+            parserOptions: {
+                requireConfigFile: false,
+                babelOptions: {
+                    presets: ['@babel/preset-react']
+                }
+            }
+        }
+    },
+    {
+        plugins: {
+            react: eslintPluginReact,
+            'react-hooks': eslintPluginReactHooks
+        },
+        rules: {
+            'react/jsx-uses-react': 'error',
+            'react/jsx-uses-vars': 'error',
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn'
+        }
+    }
+];
